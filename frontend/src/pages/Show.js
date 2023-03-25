89c2d7f472c53d7f067f36d249fd3c816e5353f8
Sergio <smontufar250@gmail.com>
2023-03-24 20:55:34 -0400

    added functionality to Show page, but without updating functionality

--- frontend/src/pages/Show.js
+++ frontend/src/pages/Show.js
@@ -1,5 +1,44 @@
+import { useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
 const Show = (props) => {
-  return (<h1>Show Page</h1>)
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const blogs = props.blogs
+  const blog = blogs.find((b) => b._id === id)
+
+  // const [editForm, setEditForm] = useState(blog)
+
+  // const handleChange = (e) => {
+  //   setEditForm(prev => ({
+  //     ...prev,
+  //     [e.target.title]: e.target.value
+  //   }))
+  // }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   props.updateBlogss(editForm, id)
+  //   navigate("/")
+  // }
+
+  const handleDelete = () => {
+    props.deleteBlogs(id)
+    navigate("/")
+  }
+
+  return (
+    <div className="blog">
+      <h1>{blog.title}</h1>
+      <p>{blog.description}</p>
+      {props.image ? 
+      <img src={blog.image} alt={blog.title} />
+      : <h1></h1>}
+      <button id="delete" onClick={handleDelete}>
+        Delete
+      </button>
+    </div>
+  )
 }
 
 export default Show;
